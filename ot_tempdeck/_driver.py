108a9b0a681cfdce602766137e7bc1a637183d03
Greg Courville <greg.courville@czbiohub.org>
2024-06-12 14:43:37 -0700

    docstrings

--- ot_tempdeck/_driver.py
+++ ot_tempdeck/_driver.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import serial
 import serial.tools.list_ports
@@ -10,6 +10,8 @@ __all__ = ["TempdeckControl"]
 
 
 class TempdeckControl:
+    """Driver for communicating with an Opentrons Tempdeck"""
+
     TempdeckControlError = TempdeckControlError
     DeviceNotFound = DeviceNotFound
     InvalidResponse = InvalidResponse
@@ -19,18 +21,23 @@ class TempdeckControl:
     USB_PID = 0xee93
 
     def __init__(self, ser_port: serial.Serial):
+        """
+        :param ser_port: a ``serial.Serial`` instance (or other object with a
+            compatible interface) that will be used to communicate with the
+            Tempdeck
+        """
         self.ser_port = ser_port
 
     @classmethod
-    def list_connected_devices(cls) -> List[str]:
-        return [
-            info.device
-            for info in serial.tools.list_ports.comports()
-            if info.vid == cls.USB_VID and info.pid == cls.USB_PID
-            ]
-
-    @classmethod
     def open_first_device(cls, *args, **kwargs) -> 'TempdeckControl':
+        """
+        Look for any connected Tempdecks (see :meth:`list_connected_devices`)
+        and open the first one found.
+
+        :raises DeviceNotFound: If no tempdecks were found
+        :raises serial.SerialException: If something went wrong opening the
+            serial device
+        """
         portnames = cls.list_connected_devices()
         if not portnames:
             raise cls.DeviceNotFound("No tempdeck detected")
@@ -39,8 +46,37 @@ class TempdeckControl:
     @classmethod
     def from_serial_portname(cls, portname: str, *args, **kwargs
                              ) -> 'TempdeckControl':
+        """
+        Open the specified serial port and initialize a new
+        :class:`TempdeckControl`.
+
+        :param portname: Port name or URL to pass to ``serial.Serial()``
+
+        :returns: New :class:`TempdeckControl` instance
+
+        :raises serial.SerialException: If something went wrong opening the
+            serial device
+        """
         ser_port = serial.Serial(portname, timeout=0.5, xonxoff=True)
         return cls(ser_port, *args, **kwargs)
+
+    @classmethod
+    def list_connected_devices(cls) -> List[str]:
+        """
+        Get a list of serial port names corresponding to Opentrons Tempdecks.
+        Checks for virtual serial ports corresponding to USB devices with the
+        appropriate VID/PID.
+
+        Only supported on Windows, macOS and Linux.
+
+        :returns: A list of serial port device names
+            (naming convention is platform-dependent)
+        """
+        return [
+            info.device
+            for info in serial.tools.list_ports.comports()
+            if info.vid == cls.USB_VID and info.pid == cls.USB_PID
+            ]
 
     def _send_command(self, cmd: str):
         self.ser_port.write(f"{cmd}\r\n".encode('ascii'))
@@ -59,10 +95,29 @@ class TempdeckControl:
                     f"Unexpected response when we expected 'ok': {line!r}")
 
     def set_target_temp(self, temp: float):
+        """
+        Set the Tempdeck's target temperature to the supplied value and
+        activate it if it wasn't active.
+
+        :param temp: Target temperature in °C
+
+        :raises ResponseTimeout, InvalidResponse: see class descriptions
+        """
         self._send_command(f"M104 S{temp:.3f}")
         self._wait_for_ack()
 
-    def get_temps(self) -> Tuple[float, float]:
+    def get_temps(self) -> Tuple[Optional[float], float]:
+        """
+        Get the current temperature setpoint and measured temperature from the
+        Tempdeck.
+
+        :returns: A tuple ``(target_temp, current_temp)`` containing the
+            current setpoint and measured temperature, respectively, in °C.
+            ``target_temp`` will be ``None`` if the tempdeck is currently
+            deactivated.
+
+        :raises ResponseTimeout, InvalidResponse: see class descriptions
+        """
         self._send_command(f"M105")
         response = self._read_line().strip()
         target_temp = None
@@ -88,13 +143,39 @@ class TempdeckControl:
         self._wait_for_ack()
         return (target_temp if target_temp != "none" else None, current_temp)
 
-    def get_target_temp(self) -> float:
+    def get_target_temp(self) -> Optional[float]:
+        """
+        Read the current temperature setpoint from the Tempdeck.
+
+        Note: If you need both the target temperature and the current measured
+        temperature it is more efficient to call :meth:`get_temps`.
+
+        :returns: The current temperature setpoint in °C, or ``None`` if the
+            tempdeck is currently deactivated
+
+        :raises ResponseTimeout, InvalidResponse: see class descriptions
+        """
         target_temp, current_temp = self.get_temps()
         return target_temp
 
     def get_current_temp(self) -> float:
+        """
+        Read the current measured temperature from the Tempdeck.
+
+        Note: If you need both the target temperature and the current measured
+        temperature it is more efficient to call :meth:`get_temps`.
+
+        :returns: The current measured temperature in °C
+
+        :raises ResponseTimeout, InvalidResponse: see class descriptions
+        """
         target_temp, current_temp = self.get_temps()
         return current_temp
 
     def deactivate(self):
+        """
+        Clear the temperature setpoint and deactivate heating and cooling.
+
+        :raises ResponseTimeout, InvalidResponse: see class descriptions
+        """
         self._send_command("M18")
--- ot_tempdeck/types.py
+++ ot_tempdeck/types.py
@@ -1,14 +1,27 @@
 class TempdeckControlError(Exception):
+    """Base class for TempdeckControl errors."""
     pass
 
 
 class DeviceNotFound(TempdeckControlError):
+    """Indicates that no tempdecks were detected."""
     pass
 
 
 class InvalidResponse(TempdeckControlError):
+    """
+    Raised when the response received to a command doesn't match the expected
+    format.
+    """
     pass
 
 
 class ResponseTimeout(TempdeckControlError):
+    """
+    Raised if the remote device doesn't send back a line of text within a
+    prescribed period of time.
+
+    One common situatuation that can cause this is accidentally connecting
+    to a serial device that's not a Tempdeck.
+    """
     pass
